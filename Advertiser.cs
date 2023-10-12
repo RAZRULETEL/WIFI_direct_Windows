2a837ce97f5f3ee1c99332117902659ab5d27fb6
RAZRULETEL <qwerty.qwertlain@gmail.com>
2023-10-12 23:36:39 +0300

    Code cleanup, added README, rename MiniAdvertiser to Advertiser

--- Advertiser.cs
+++ Advertiser.cs
@@ -1,36 +1,35 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using SDKTemplate;
 using Windows.Devices.Enumeration;
 using Windows.Devices.WiFiDirect;
-using Windows.Foundation;
+using Windows.Networking;
 using Windows.Networking.Sockets;
-using Windows.UI.Popups;
-using System.Threading;
-using System.Diagnostics;
 
-namespace SDKTemplate
+namespace WiFiDirectApi
 {
+
     public class Advertiser
     {
         private WiFiDirectAdvertisementPublisher publisher;
         private WiFiDirectConnectionListener listener;
-
+        private SocketReaderWriter socketRW;
         public Advertiser()
         {
-
         }
-
-        public bool startAdvertisement()
+        public bool StartAdvertisement()
         {
             publisher = new WiFiDirectAdvertisementPublisher();
+            publisher.StatusChanged += OnStatusChanged;
+
             listener = new WiFiDirectConnectionListener();
 
-            var discoverability = WiFiDirectAdvertisementListenStateDiscoverability.Normal;
+            var discoverability = WiFiDirectAdvertisementListenStateDiscoverability.Intensive;
             publisher.Advertisement.ListenStateDiscoverability = discoverability;
+
+            publisher.Advertisement.IsAutonomousGroupOwnerEnabled = true;
 
             try
             {
@@ -43,20 +42,24 @@ namespace SDKTemplate
                 return false;
             }
 
-            publisher.StatusChanged += OnStatusChanged;
-
             publisher.Start();
 
             return publisher.Status == WiFiDirectAdvertisementPublisherStatus.Started;
         }
 
-        public void stopAdvertisement()
+        public bool StopAdvertisement()
         {
             publisher.Stop();
 
-            publisher.StatusChanged -= OnStatusChanged;
+            if (publisher.Status == WiFiDirectAdvertisementPublisherStatus.Stopped)
+            {
 
-            listener.ConnectionRequested -= OnConnectionRequested;
+                publisher.StatusChanged -= OnStatusChanged;
+
+                listener.ConnectionRequested -= OnConnectionRequested;
+                return true;
+            }
+            return false;
         }
 
         private void OnStatusChanged(WiFiDirectAdvertisementPublisher sender, WiFiDirectAdvertisementPublisherStatusChangedEventArgs statusEventArgs)
@@ -64,16 +67,19 @@ namespace SDKTemplate
             Debug.WriteLine(statusEventArgs.Status);
         }
 
-            private async void OnConnectionRequested(WiFiDirectConnectionListener sender, WiFiDirectConnectionRequestedEventArgs connectionEventArgs)
+        private async void OnConnectionRequested(WiFiDirectConnectionListener sender, WiFiDirectConnectionRequestedEventArgs connectionEventArgs)
         {
+            Debug.WriteLine("Connection requested!!!");
             WiFiDirectConnectionRequest connectionRequest = connectionEventArgs.GetConnectionRequest();
-            bool success = //await Dispatcher.RunTaskAsync(async () =>{
-                /*return*/ await HandleConnectionRequestAsync(connectionRequest);
-            //});
+            bool success = await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunTaskAsync(
+                async () =>
+                {
+                    return await HandleConnectionRequestAsync(connectionRequest);
+                });
 
             if (!success)
             {
-                // Decline the connection request
+                Debug.WriteLine("Pair request was declined !!!");
                 connectionRequest.Dispose();
             }
         }
@@ -85,30 +91,16 @@ namespace SDKTemplate
             bool isPaired = (connectionRequest.DeviceInformation.Pairing?.IsPaired == true) ||
                             (await IsAepPairedAsync(connectionRequest.DeviceInformation.Id));
 
+            Debug.WriteLine($"Connecting to {deviceName}...", NotifyType.StatusMessage);
+
             // Show the prompt only in case of WiFiDirect reconnection or Legacy client connection.
             if (isPaired || publisher.Advertisement.LegacySettings.IsEnabled)
             {
-                var messageDialog = new MessageDialog($"Connection request received from {deviceName}", "Connection Request");
-
-                // Add two commands, distinguished by their tag.
-                // The default command is "Decline", and if the user cancels, we treat it as "Decline".
-                messageDialog.Commands.Add(new UICommand("Accept", null, true));
-                messageDialog.Commands.Add(new UICommand("Decline", null, null));
-                messageDialog.DefaultCommandIndex = 1;
-                messageDialog.CancelCommandIndex = 1;
-
-                // Show the message dialog
-                var commandChosen = await messageDialog.ShowAsync();
-
-                if (commandChosen.Id == null)
-                {
-                    return false;
-                }
+                Debug.WriteLineIf(isPaired, "Reconnect!");
+                Debug.WriteLineIf(publisher.Advertisement.LegacySettings.IsEnabled, "Legacy connection!");
             }
 
-            Debug.WriteLine($"Connecting to {deviceName}...", NotifyType.StatusMessage);
-
-            // Pair device if not already paired and not using legacy settings
+                // Pair device if not already paired and not using legacy settings
             if (!isPaired && !publisher.Advertisement.LegacySettings.IsEnabled)
             {
                 if (!await RequestPairDeviceAsync(connectionRequest.DeviceInformation.Pairing))
@@ -129,13 +121,9 @@ namespace SDKTemplate
                 return false;
             }
 
-            // Register for the ConnectionStatusChanged event handler
-            //wfdDevice.ConnectionStatusChanged += OnConnectionStatusChanged;
+            wfdDevice.ConnectionStatusChanged += OnConnectionStatusChanged;
 
             var listenerSocket = new StreamSocketListener();
-
-            // Save this (listenerSocket, wfdDevice) pair so we can hook it up when the socket connection is made.
-            //_pendingConnections[listenerSocket] = wfdDevice;
 
             var EndpointPairs = wfdDevice.GetConnectionEndpointPairs();
 
@@ -150,11 +138,38 @@ namespace SDKTemplate
                 return false;
             }
 
+            //RequestSocketTransfer(wfdDevice);
+
             Debug.WriteLine($"Devices connected on L2, listening on IP Address: {EndpointPairs[0].LocalHostName}" +
                                 $" Port: {Globals.strServerPort}", NotifyType.StatusMessage);
             return true;
         }
 
+        public async Task<bool> RequestPairDeviceAsync(DeviceInformationPairing pairing)
+        {
+            Debug.WriteLine("Trying pair device async");
+            WiFiDirectConnectionParameters connectionParams = new WiFiDirectConnectionParameters();
+            DevicePairingKinds devicePairingKinds = DevicePairingKinds.ConfirmOnly | DevicePairingKinds.ConfirmPinMatch
+                | DevicePairingKinds.DisplayPin;
+
+            connectionParams.PreferredPairingProcedure = WiFiDirectPairingProcedure.GroupOwnerNegotiation;
+            DeviceInformationCustomPairing customPairing = pairing.Custom;
+            customPairing.PairingRequested += OnPairingRequested;
+
+            DevicePairingResult result = await customPairing.PairAsync(devicePairingKinds, DevicePairingProtectionLevel.None, connectionParams);
+            if (result.Status != DevicePairingResultStatus.Paired)
+            {
+                Debug.WriteLine($"PairAsync failed, Status: {result.Status}", NotifyType.ErrorMessage);
+                return false;
+            }
+            return true;
+        }
+
+        private void OnPairingRequested(DeviceInformationCustomPairing sender, DevicePairingRequestedEventArgs args)
+        {
+            Debug.WriteLine("On pairing request " + args.PairingKind);
+            Utils.HandlePairing(Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher, args);
+        }
         private async Task<bool> IsAepPairedAsync(string deviceId)
         {
             List<string> additionalProperties = new List<string>();
@@ -179,120 +194,100 @@ namespace SDKTemplate
 
             deviceSelector = $"System.Devices.Aep.DeviceAddress:=\"{devInfo.Properties["System.Devices.Aep.DeviceAddress"]}\"";
             DeviceInformationCollection pairedDeviceCollection = await DeviceInformation.FindAllAsync(deviceSelector, null, DeviceInformationKind.Device);
+            Debug.WriteLine("Paired devices " + pairedDeviceCollection.Count + ": ");
+            foreach (DeviceInformation dev in pairedDeviceCollection)
+            {
+                Debug.Write(dev);
+            }
             return pairedDeviceCollection.Count > 0;
         }
 
-
-        public async Task<bool> RequestPairDeviceAsync(DeviceInformationPairing pairing)
+        private void OnSocketConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
         {
-            WiFiDirectConnectionParameters connectionParams = new WiFiDirectConnectionParameters();
-
-            /*short? groupOwnerIntent = 0;//Utils.GetSelectedItemTag<short?>(cmbGOIntent);
-            if (groupOwnerIntent.HasValue)
+            var task = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunTaskAsync(
+            async () =>
             {
-                connectionParams.GroupOwnerIntent = groupOwnerIntent.Value;
-            }*/
-
-            DevicePairingKinds devicePairingKinds = DevicePairingKinds.None;
-
-            // If specific configuration methods were added, then use them.
-/*            if (supportedConfigMethods.Count > 0)
-            {
-                foreach (var configMethod in supportedConfigMethods)
-                {
-                    connectionParams.PreferenceOrderedConfigurationMethods.Add(configMethod);
-                    devicePairingKinds |= WiFiDirectConnectionParameters.GetDevicePairingKinds(configMethod);
-                }
-            }
-            else
-            {*/
-                // If specific configuration methods were not added, then we'll use these pairing kinds.
-                devicePairingKinds = DevicePairingKinds.ConfirmOnly | DevicePairingKinds.DisplayPin | DevicePairingKinds.ProvidePin;
-           /* }*/
-
-            Debug.WriteLine(devicePairingKinds.ToString());
-
-            connectionParams.PreferredPairingProcedure = WiFiDirectPairingProcedure.GroupOwnerNegotiation;//Utils.GetSelectedItemTag<WiFiDirectPairingProcedure>(cmbPreferredPairingProcedure);
-            DeviceInformationCustomPairing customPairing = pairing.Custom;
-            customPairing.PairingRequested += OnPairingRequested;
-
-            DevicePairingResult result = await customPairing.PairAsync(devicePairingKinds, DevicePairingProtectionLevel.Default, connectionParams);
-            if (result.Status != DevicePairingResultStatus.Paired)
-            {
-                Debug.WriteLine($"PairAsync failed, Status: {result.Status}", NotifyType.ErrorMessage);
-                return false;
-            }
-            return true;
-        }
-
-        private void OnPairingRequested(DeviceInformationCustomPairing sender, DevicePairingRequestedEventArgs args)
-        {
-            HandlePairing(args);
-        }
-
-        private static void HandlePairing(DevicePairingRequestedEventArgs args)
-        {
-            using (Deferral deferral = args.GetDeferral())
-            {
-                Debug.WriteLine("Handle pairing");
-                switch (args.PairingKind)
-                {
-                    case DevicePairingKinds.DisplayPin:
-                        //await ShowPinToUserAsync(dispatcher, args.Pin);
-                        Debug.WriteLine(args.Pin);
-                        args.Accept();
-                        break;
-
-                    case DevicePairingKinds.ConfirmOnly:
-                        Debug.WriteLine("Accept pin");
-                        args.Accept();
-                        break;
-
-                    case DevicePairingKinds.ProvidePin:
-                        {
-                            Debug.WriteLine("Provide pin");
-                            /*string pin = await GetPinFromUserAsync(dispatcher);
-                            if (!String.IsNullOrEmpty(pin))
-                            {
-                                args.Accept(pin);
-                            }*/
-                        }
-                        break;
-                }
-            }
-        }
-
-        private async void OnSocketConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
-        {
-            //var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
-            //Dispatcher(async () => {
                 Debug.WriteLine("Connecting to remote side on L4 layer...", NotifyType.StatusMessage);
                 StreamSocket serverSocket = args.Socket;
 
-                // Look up the WiFiDirectDevice associated with this StreamSocketListener.
-                WiFiDirectDevice wfdDevice;
-                /*if (!_pendingConnections.TryRemove(sender, out wfdDevice))
-                {
-                    rootPage.NotifyUser("Unexpected connection ignored.", NotifyType.ErrorMessage);
-                    serverSocket.Dispose();
-                    return;
-                }*/
+                socketRW = new SocketReaderWriter(serverSocket, null);
 
-                SocketReaderWriter socketRW = new SocketReaderWriter(serverSocket, null);
+                await socketRW.WriteMessageAsync("Hello from Moon ");
 
                 // The first message sent is the name of the connection.
                 string message = await socketRW.ReadMessageAsync();
 
-            // Add this connection to the list of active connections.
-            //ConnectedDevices.Add(new ConnectedDevice(message ?? "(unnamed)", wfdDevice, socketRW));
-            Debug.WriteLine(message ?? "(unnamed)");
+                // Add this connection to the list of active connections.
+                //ConnectedDevices.Add(new ConnectedDevice(message ?? "(unnamed)", wfdDevice, socketRW));
+                Debug.WriteLine(message ?? "(unnamed)", NotifyType.StatusMessage);
+
+                while (message != null)
+                {
+                    message = await socketRW.ReadMessageAsync();
+                    Debug.WriteLine($"{message}", NotifyType.StatusMessage);
+                }
+            });
+        }
+
+        private void OnConnectionStatusChanged(WiFiDirectDevice sender, object arg)
+        {
+            Debug.WriteLine($"Connection status changed: {sender.ConnectionStatus}", NotifyType.StatusMessage);
+
+        }
+
+        private void RequestSocketTransfer(WiFiDirectDevice wfdDevice)
+        {
+            var task = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunTaskAsync(
+            async () =>
+            {
+                IReadOnlyList<EndpointPair> endpointPairs = wfdDevice.GetConnectionEndpointPairs();
+                HostName remoteHostName = endpointPairs[0].RemoteHostName;
+
+                Debug.WriteLine($"Devices connected on L2 layer, connecting to IP Address: {remoteHostName} Port: {Globals.strServerPort}",
+                    NotifyType.StatusMessage);
+
+                // Wait for server to start listening on a socket
+                await Task.Delay(2_000);
+
+                // Connect to Advertiser on L4 layer
+                StreamSocket clientSocket = new StreamSocket();
+                try
+                {
+                    await clientSocket.ConnectAsync(remoteHostName, Globals.strServerPort);
+                    Debug.WriteLine("Connected with remote side on L4 layer", NotifyType.StatusMessage);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Connect operation threw an exception: {ex.Message}", NotifyType.ErrorMessage);
+                    return;
+                }
+
+                socketRW = new SocketReaderWriter(clientSocket, null);
+
+                await socketRW.WriteMessageAsync("Hello from Moon ");
+
+                string message = await socketRW.ReadMessageAsync();
+
+                Debug.WriteLine(message ?? "(unnamed)");
 
                 while (message != null)
                 {
                     message = await socketRW.ReadMessageAsync();
                     Debug.WriteLine($"{message}");
                 }
-            //});
+
+            });
+        }
+
+        public async void SendMessage(String msg)
+        {
+            if (socketRW != null)
+            {
+                await socketRW.WriteMessageAsync(msg);
+            }
+            else{
+                Debug.WriteLine("Socket is null!");
+            }
         }
     }
 }
